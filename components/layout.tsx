9e285858d76a1fe5f2409110f1e8fac3db709917
rami6 <30137645+rami6@users.noreply.github.com>
2021-04-17 18:18:09 -0700

    Make the index page initial state

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-export default function Layout({ children }) {
+export default function Layout({ children, pageTitle = 'Default Page Title' }) {
   return (
     <div className={styles.container}>
       <Head>
         <title>Kingdom Death Note</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header>
+        <h2>{pageTitle}</h2>
+      </header>
       <main className={styles.main}>{children}</main>
     </div>
   );
